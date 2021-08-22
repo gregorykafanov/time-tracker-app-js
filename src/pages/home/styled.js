28ff4fe8ae2bb3e2c50b588bf65cea77a1faf015
gregorykafanov <gregkaf@gmail.com>
2021-08-22 21:17:27 +0300

    Updates

--- src/pages/home/styled.js
+++ src/pages/home/styled.js
@@ -3,10 +3,3 @@ import styled from 'styled-components/macro';
 export const Wrapper = styled.div`
   padding-top: 20px;
 `;
-
-// export const Header = styled.div`
-//   font-family: sans-serif;
-//   font-size: 50px;
-//   text-align: center;
-//   margin-bottom: 20px;
-// `;
