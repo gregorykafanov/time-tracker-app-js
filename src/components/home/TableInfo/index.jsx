8971aca7ae697a419f864b050bf08995516f3a10
gregorykafanov <gregkaf@gmail.com>
2021-08-22 12:54:28 +0300

    Updates

---
+++ src/components/home/TableInfo/index.jsx
@@ -0,0 +1,3 @@
+export default function TableInfo() {
+  return <div></div>;
+}
--- src/components/home/TrackPopup/index.jsx
+++ src/components/home/TrackPopup/index.jsx
@@ -2,15 +2,15 @@ import { Body, Button, Footer, Frame, Header, Wrapper } from './styled';
 
 export default function TrackPopup({ setIsPopupVisibleCb }) {
   return (
-    <Wrapper>
-      <Frame>
+    <Wrapper onClick={() => setIsPopupVisibleCb(false)}>
+      <Frame onClick={e => e.stopPropagation()}>
         <Header>Track User</Header>
 
         <Body>
           <div>Name: Jim Morris</div>
           <div>Project Time: 17 hours</div>
           <div>Note:</div>
-          <textarea name="" id="" cols="30" rows="10"></textarea>
+          <textarea name="popup" cols="30" rows="10" />
           <div>
             Tracked: <input type="checkbox" />
           </div>
